bbf8bf653b7c92a5559bc85082770de5b1844379
techfusion-hyd <techfusionhyd@gmail.com>
2016-09-12 20:40:25 +0530

    Environment Setup. Added: 1. RequireJS 2. Bootstrap 3. JQuery 4. Angular

--- js/Main.js
+++ js/Main.js
@@ -1,10 +1,19 @@
 /**
  * Created by nikhileshc on 9/12/2016.
  */
-/*global requirejs,menuItems*/
+/*global requirejs,menuItems,$,jQuery*/
 if (requirejs) {
-    requirejs(['config'], function(config) {
-        console.log(menuItems);
+    require.config({
+        paths: {
+            config: 'config',
+            jQuery: 'jQuery/jquery.min',
+            angularJs: 'AngularJS/angular.min',
+            bootstrap: 'bootstrap-3.3.7-dist/js/bootstrap.min'
+        }
+    });
+    //now load the dependencies accordingly
+    requirejs(['config','jQuery', 'angularJs', 'bootstrap'], function(config) {
+        console.log(config, menuItems, $('html'), angular);
     });
 } else {
     console.log('You do not have require JS initialised');
